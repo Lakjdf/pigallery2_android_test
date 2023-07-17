7157e67169ea600659d378db15208cf1f7f3e3b7
Lakjdf <13982482+Lakjdf@users.noreply.github.com>
2023-07-18 00:04:23 +0200

    use dynamic theme for search bar

--- lib/ui/views/home/search/gallery_search_delegate.dart
+++ lib/ui/views/home/search/gallery_search_delegate.dart
@@ -5,6 +5,24 @@ import 'package:provider/provider.dart';
 
 class GallerySearchDelegate extends SearchDelegate<String> {
   @override
+  ThemeData appBarTheme(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return theme.copyWith(
+      appBarTheme: AppBarTheme(
+        backgroundColor: theme.appBarTheme.backgroundColor,
+        titleTextStyle: theme.textTheme.titleLarge,
+        toolbarTextStyle: theme.textTheme.bodyMedium,
+        iconTheme: theme.iconTheme,
+      ),
+      inputDecorationTheme: searchFieldDecorationTheme ??
+          InputDecorationTheme(
+            hintStyle: searchFieldStyle ?? theme.inputDecorationTheme.hintStyle,
+            border: InputBorder.none,
+          ),
+    );
+  }
+
+  @override
   void close(BuildContext context, String result) {
     HomeModel model = Provider.of<HomeModel>(context, listen: false);
     model.stopSearch();
